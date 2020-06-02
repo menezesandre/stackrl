Change metrics to be averaged from a buffer of fixed length.

siamrl/train.py
@@ -18,7 +18,8 @@ class Training(object):
     eval_env=None,
     net=PseudoSiamFCN,
     agent=DQN,
-    num_eval_episodes=10,
+    train_reward_buffer_length=10,
+    eval_reward_buffer_length=10,
     directory='.',
     save_evaluated_policies=False,
     log_to_file=True,
@@ -38,7 +39,10 @@ class Training(object):
         tuple with input shape as argument.
       agent: constructor for the agent. Receives the Q network as 
         argument.
-      num_eval_episodes: number of episodes to run on policy evaluation.
+      train_reward_buffer_length: train reward logged is the average of 
+        the rewards from this number of most recent episodes. 
+      eval_reward_buffer_length: number of episodes to run on policy 
+        evaluation.
       directory: path to the directory where checkpoints, models and logs
         are to be saved
       save_evaluated_policies: whether to save the agent's net weights
@@ -109,14 +113,17 @@ class Training(object):
     # Evaluation log
     self._eval_interval = eval_interval
     self._eval_file = os.path.join(directory, 'eval.csv')    
-    self._num_eval_episodes = tf.constant(num_eval_episodes, dtype=tf.int32)
     
     # Metrics
-    self._reward = AverageReward(self._env.batch_size)
-    self._avg_reward = AverageReward(self._env.batch_size)
-    self._eval_reward = AverageReward(self._eval_env.batch_size)
-    self._loss = AverageMetric()
-    self._mean_error = AverageMetric()
+    self._reward = AverageReward(
+      self._env.batch_size,
+      length=train_reward_buffer_length)
+    self._eval_reward = AverageReward(
+      self._eval_env.batch_size,
+      length=eval_reward_buffer_length
+    )
+    self._loss = AverageMetric(length=log_interval)
+    self._mean_error = AverageMetric(length=log_interval)
     self._collect_timer = Timer()
     self._train_timer = Timer()
 
@@ -133,10 +140,7 @@ class Training(object):
     self._checkpoint_interval = checkpoint_interval
     self._checkpoint = tf.train.Checkpoint(
       agent=self._agent,
-      reward=self._reward,
-      avg_reward=self._avg_reward,
-      loss=self._loss,
-      mean_error=self._mean_error
+      reward=self._reward
     )
     self._checkpoint_manager = tf.train.CheckpointManager(
       self._checkpoint,
@@ -226,10 +230,7 @@ class Training(object):
         with self._collect_timer:
           if callable(step):
             step = step() # pylint: disable=not-callable
-
-          self._avg_reward += step
           self._reward += step
-
           action = self._agent.collect(*step)
           step = self._env.step(action)
         
@@ -262,7 +263,7 @@ class Training(object):
     # Reset evaluation reward and environment
     self._eval_reward.reset(full=True)
     step = self._eval_env.reset()
-    while self._eval_reward.episode_count < self._num_eval_episodes:
+    while not self._eval_reward.full:
       if callable(step):
         o = step()[0]
       else:
@@ -320,18 +321,14 @@ class Training(object):
     reward = self._reward.result.numpy()
     loss = self._loss.result.numpy()
     merr = self._mean_error.result.numpy()
-    self._reward.reset()
-    self._loss.reset()
-    self._mean_error.reset()
 
     # If file doesn't exist, write header
     if not os.path.isfile(self._train_file):
-      line = 'Iter,AvgReward,Reward,Loss,MeanError,CollectTime,TrainTime\n'
+      line = 'Iter,Reward,Loss,MeanError,CollectTime,TrainTime\n'
     else:
       line = ''
-    line += '{},{},{},{},{},{},{}\n'.format(
+    line += '{},{},{},{},{},{}\n'.format(
       iters,
-      self._avg_reward.result.numpy(),
       reward,
       loss,
       merr,
@@ -341,7 +338,7 @@ class Training(object):
     with open(self._train_file, 'a') as f:
       f.write(line)
     
-    self.log('Iter {:8} Reward {:12.7} Loss {:12.7}'.format(iters,reward,loss))
+    self.log('Iter {:8} Reward {:<11.6} Loss {:<11.6}'.format(iters,reward,loss))
 
   def log_exception(self):
     """Logs the last exception's traceback with a timestamp"""

siamrl/utils.py
@@ -88,31 +88,42 @@ class Timer(object):
     self.n = 0
 
 class AverageMetric(tf.Module):
-  def __init__(self, dtype=tf.float32):
+  def __init__(self, length=100, dtype=tf.float32):
     super(AverageMetric, self).__init__(name='Average')
     self._dtype = tf.dtypes.as_dtype(dtype)
+    self._length = length
 
-    self._total = tf.Variable(tf.zeros((), dtype=self._dtype))
-    self._count = tf.Variable(0, dtype=tf.int32)
-
-  def __call__(self, value):
-    self._total.assign_add(value)
-    self._count.assign_add(1)
+    self._index = tf.Variable(0, dtype=tf.int32)
+    self._values = tf.Variable(tf.zeros((length,), dtype=self._dtype))
 
   @property
   def result(self):
-    return self._total/tf.cast(self._count, dtype=self._dtype)
+    return tf.reduce_mean(self._values) if self.full else \
+      tf.reduce_sum(self._values)/tf.cast(self._index, dtype=self._dtype)
+  @property
+  def full(self):
+    return self._index >= self._length
+
+  def add(self, value):
+    self._values.scatter_update(tf.IndexedSlices(
+      value,
+      self._index%self._length
+    ))
+    self._index.assign_add(1)
 
   def reset(self):
     """Reset metric."""
-    self._total.assign(tf.zeros_like(self._total))
-    self._count.assign(tf.zeros_like(self._count))
+    self._values.assign(tf.zeros_like(self._values))
+    self._index.assign(tf.zeros_like(self._index))
 
+  # Aliases of add.
+  def __call__(self, *args, **kwargs):
+    """Calling does the same as add"""
+    return self.add(*args, **kwargs)
   def __iadd__(self, other):
-    """Inplace addition does the same as calling."""
-    self.__call__(other)
+    """Inplace adition does the same as add"""
+    self.add(other)
     return self
-
   # Comparison operations
   def __lt__(self, other):
     return self.result < other
@@ -125,21 +136,20 @@ class AverageMetric(tf.Module):
   def __gt__(self, other):
     return self.result > other
 
-
 class AverageReward(AverageMetric):
-  def __init__(self, batch_size, dtype=tf.float32):
+  def __init__(self, batch_size, length=100, dtype=tf.float32):
     """
     Args:
       batch_size: of the rewards (and terminal flags) to be received by
       __call__().
     """
-    super(AverageReward, self).__init__(dtype=dtype)
+    super(AverageReward, self).__init__(length=length, dtype=dtype)
     self._episode_reward = tf.Variable(tf.zeros(
       (batch_size,), 
       dtype=self._dtype
     ))
   
-  def __call__(self, step):
+  def add(self, step):
     """Adds a step's reward to the metric.
     Args:
       step: collection in which the last two elements are a batch of 
@@ -148,35 +158,10 @@ class AverageReward(AverageMetric):
     terminal = step[-1]
     # Add this step's reward to the episode reward
     self._episode_reward.assign_add(reward)
-
-    terminal_count = tf.math.count_nonzero(terminal, dtype=tf.int32)
-    terminal_indexes = tf.where(terminal)
-    # Add the number of finished episodes to episode count
-    self._count.assign_add(terminal_count)
-    # Add the rewards from finished episodes to the total reward
-    self._total.assign_add(tf.reduce_sum(
-      self._episode_reward.sparse_read(terminal_indexes)
-    ))
-    # Reset the reward from finished episodes
-    self._episode_reward.scatter_nd_update(
-      terminal_indexes, 
-      tf.zeros(terminal_count, dtype=tf.float32)
-    )
-    # Alternative (TODO check if it's faster)
-    # self._total.assign_add(tf.reduce_sum(tf.where(
-    #   terminal,
-    #   self._episode_reward,
-    #   0.
-    # )))
-    # self._episode_reward.assign(tf.where(
-    #   terminal,
-    #   0.,
-    #   self._episode_reward
-    # ))
-
-  @property
-  def episode_count(self):
-    return self._count
+    # Add the rewards from the finished episodes to the buffer and reset them
+    for i in tf.squeeze(tf.where(terminal), axis=-1):
+      super(AverageReward, self).add(self._episode_reward.sparse_read(i))
+      self._episode_reward.scatter_update(tf.IndexedSlices(0, i))
   
   def reset(self, full=False):
     """Reset metric. If full is False, the rewards of the ongoing episodes