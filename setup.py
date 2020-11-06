8b80a34f7d2483b411d19e6b3c95930a009ef86f
atmenezes96 <andre.menezes@tecnico.ulisboa.pt>
2020-11-06 14:32:47 +0000

    Bug fix

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 import os
 
+path = lambda *args: os.path.join(os.path.dirname(__file__), *args)
+
 # Read version number from package __init__.py
 version = {}
-with open(os.path.join(os.path.dirname(__file__),'stackrl', '__init__.py')) as f:
+with open(path('stackrl', '__init__.py')) as f:
   for line in f:
     if '=' in line:
       line = line.split('=')
@@ -12,7 +14,7 @@ with open(os.path.join(os.path.dirname(__file__),'stackrl', '__init__.py')) as f
 version = '{MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}'.format(**version)
 
 REQUIRES = [
-  'numpy>=19.0.0',
+  'numpy>=1.19.0',
   'scipy',
   'gym', 
   'pybullet',
@@ -28,16 +30,16 @@ try:
 except (ImportError, AssertionError):
   REQUIRES.insert(0, 'tensorflow>=2.0.0')
 
-with open('README.md', encoding='utf-8') as f:
+with open(path('README.md'), encoding='utf-8') as f:
   long_description = f.read()
 
 setup(
   name='stackrl',
   version=version,
-  description='Reinforcement learning with (pseudo) siamese networks.',
+  description='Learning to dry stack with irregular blocks using Reinforcement Learning.',
   long_description=long_description,
   long_description_content_type='text/markdown',
-  url='https://github.com/atmenezes96/Siam-RL',
+  url='https://github.com/menezesandre/stackrl',
   author='André Menezes',
   author_email='andre.menezes@tecnico.ulisboa.pt',
   install_requires=REQUIRES,
@@ -48,28 +50,3 @@ setup(
     'plot': ['matplotlib'],
   },
 )
-
-# import glob
-# import sys
-
-# # Install apps
-# for fname in glob.glob('apps/*.py'):
-#   # Interpreter is the program executing this instalation
-#   shebang = '#!'+sys.executable+'\n'
-#   with open(fname,'r') as f:
-#     lines = f.readlines()
-#     # Check if shebang is correct
-#     write = lines[0] != shebang
-#   # Overwrite file if necessary
-#   if write:
-#     if lines[0].startswith('#!'):
-#       lines[0] = shebang
-#     else:
-#       lines.insert(0,shebang)
-#     with open(fname, 'w') as f:
-#       for line in lines:
-#         f.write(line)
-#   # Add execute permission to those who have read permission
-#   mode = os.stat(fname).st_mode
-#   mode |= (mode & 0o444) >> 2
-#   os.chmod(fname, mode)
